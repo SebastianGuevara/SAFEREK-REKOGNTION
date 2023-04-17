8612d8f05761fe85c0763b6317db6b37eb756d0f
SebastianGuevara <sebastianguve@unisabana.edu.co>
2023-04-16 21:53:35 -0500

    Hace prueba de funcion con env

--- src/controllers/ImageController.js
+++ src/controllers/ImageController.js
@@ -3,14 +3,15 @@ require('dotenv').config({path:'../../.env'})
 
 const getImageFromCollection = async (req,res) =>{
     const { imageName } = req.params;
-    console.log(imageName);
+    //console.log(imageName);
     res.send({workerId:await searchFacesInCollection(imageName)});
+    console.log(process.env.SECRET_ACCESS_KEY);
+    console.log(process.env.ACCESS_KEY_ID);
 }
 
 const test = ( req,res) => {
     const { word } = req.params;
     res.send({test:require('uuid').v4()});
 }
-console.log(process.env.SECRET_ACCESS_KEY);
-console.log(process.env.ACCESS_KEY_ID);
+
 module.exports = {getImageFromCollection, test};
