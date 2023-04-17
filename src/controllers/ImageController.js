b5391b8e425b3f1e91700e18b56edc11dddea4b4
SebastianGuevara <sebastianguve@unisabana.edu.co>
2023-04-16 21:47:28 -0500

    Hace prueba de funcion con env

--- src/controllers/ImageController.js
+++ src/controllers/ImageController.js
@@ -1,4 +1,5 @@
 const { searchFacesInCollection } = require('../services/aws/RekognitionServices.js')
+require('dotenv').config({path:'../../.env'})
 
 const getImageFromCollection = async (req,res) =>{
     const { imageName } = req.params;
@@ -10,5 +11,6 @@ const test = ( req,res) => {
     const { word } = req.params;
     res.send({test:require('uuid').v4()});
 }
-
+console.log(process.env.SECRET_ACCESS_KEY);
+console.log(process.env.ACCESS_KEY_ID);
 module.exports = {getImageFromCollection, test};
